f1046bf865ca17cc7a259350adf452247fa87508
jtrauer <james.trauer@monash.edu>
2024-09-25 22:16:18 +1000

    Move importance function to .py file and tidy

--- emu_filter/importance.py
+++ emu_filter/importance.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.stats import truncnorm
 
+
 # Calculation of importance weights
-def get_importance(p_vals, mean, target_sd):
+def truncnorm_importance(p_vals, mean, target_sd):
     zero_trunc_vals = -p_vals / target_sd
-    target = np.repeat(mean, n_particles)
+    target = np.repeat(mean, len(p_vals))
     return truncnorm.pdf(target, zero_trunc_vals, np.inf, loc=p_vals, scale=target_sd)
--- emu_filter/outputs.py
+++ emu_filter/outputs.py
@@ -7,15 +7,30 @@ def get_counts_from_particles(particles):
     melted_df = pd.DataFrame(particles.T).melt(var_name="Columns", value_name="Values")
     return melted_df.groupby(["Columns", "Values"]).size().reset_index(name="Counts")
 
+
 # Plot results for number of infectious from output array
 def plot_particle_results(prop_particles, resamp_particles, observations):
     prop_counts = get_counts_from_particles(prop_particles[:, 1, :])
     resamp_counts = get_counts_from_particles(resamp_particles[:, 1, :])
-    results_plot = plt.scatter(prop_counts["Columns"] - 0.2, prop_counts["Values"], s=prop_counts["Counts"] * 15.0, alpha=0.5, label="proposed")
-    plt.scatter(resamp_counts["Columns"], resamp_counts["Values"], s=resamp_counts["Counts"] * 15.0, alpha=0.5, label="resampled", color="g")
+    results_plot = plt.scatter(
+        prop_counts["Columns"] - 0.2,
+        prop_counts["Values"],
+        s=prop_counts["Counts"] * 15.0,
+        alpha=0.5,
+        label="proposed",
+    )
+    plt.scatter(
+        resamp_counts["Columns"],
+        resamp_counts["Values"],
+        s=resamp_counts["Counts"] * 15.0,
+        alpha=0.5,
+        label="resampled",
+        color="g",
+    )
     plt.scatter(range(1, len(observations) + 1), observations, label="target", color="k")
     plt.legend()
     return results_plot
+
 
 def get_links_from_pedigree(particles, pedigree, observations):
     links = []
@@ -24,9 +39,17 @@ def get_links_from_pedigree(particles, pedigree, observations):
             links.append([t, t + 1, particles[t, 1, origin], particles[t + 1, 1, dest]])
     return np.array(links)
 
+
 def plot_links(particles, links, obs):
     resamp_counts = get_counts_from_particles(particles[:, 1, :])
-    links_plot = plt.scatter(resamp_counts["Columns"], resamp_counts["Values"], s=resamp_counts["Counts"] * 15.0, alpha=0.5, label="resampled", color="g")
+    links_plot = plt.scatter(
+        resamp_counts["Columns"],
+        resamp_counts["Values"],
+        s=resamp_counts["Counts"] * 15.0,
+        alpha=0.5,
+        label="resampled",
+        color="g",
+    )
     for l in links:
         plt.plot(l[:2], l[2:], color="gray", linewidth=0.1, label=None)
     plt.scatter(range(1, len(obs) + 1), obs, label="target", color="k")
