5e5a753df98bc30af5a33e55238c7d562b3af34f
jtrauer <james.trauer@monash.edu>
2025-11-24 06:54:59 +1100

    Define variable for force of infection in state update

--- emu_filter/state_model.py
+++ emu_filter/state_model.py
@@ -5,7 +5,8 @@ import numpy as np
 def predict_states(particles, contact_rate, recovery_rate, total_pop):
     updated_particles = np.empty_like(particles)
     suscept, infect, recovered = particles[0, :], particles[1, :], particles[2, :]
-    new_infections = np.random.binomial(suscept, 1.0 - np.exp(-contact_rate * infect / total_pop))
+    force_infection = contact_rate * infect / total_pop
+    new_infections = np.random.binomial(suscept, 1.0 - np.exp(-force_infection))
     new_recoveries = np.random.binomial(infect, 1.0 - np.exp(-recovery_rate))
 
     updated_particles[0, :] = suscept - new_infections
